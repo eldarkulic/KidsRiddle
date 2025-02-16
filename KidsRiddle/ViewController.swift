87bc86325da996205e2ff3042015292675b87136
Eldar <eldar@Amers-MacBook-Pro.local>
2025-02-16 11:14:33 +0100

    Added settings button

--- KidsRiddle/ViewController.swift
+++ KidsRiddle/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var currentRiddleIndex: Int = 0
     var isAnswerRevealed: Bool = false
     
+    
     // UI Elements
     let riddleLabel: UILabel = {
         let label = UILabel()
@@ -38,13 +39,25 @@ class ViewController: UIViewController {
         imageView.contentMode = .scaleToFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.cornerRadius = 15
-    //    imageView.clipsToBounds = true
+        //    imageView.clipsToBounds = true
         
         imageView.layer.shadowColor = UIColor.black.cgColor
         imageView.layer.shadowOpacity = 0.35
         imageView.layer.shadowOffset = CGSize(width: 5, height: 5)
         imageView.layer.shadowRadius = 8
         return imageView
+    }()
+    
+    let settingsButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let image = UIImage(systemName: "gearshape")
+        button.setImage(image, for: .normal)
+        
+        button.tintColor = .white
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     let showButton: UIButton = {
@@ -83,7 +96,7 @@ class ViewController: UIViewController {
         
         return button
     }()
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -96,6 +109,7 @@ class ViewController: UIViewController {
         view.addSubview(nextButton)
         view.addSubview(answerLabel)
         view.addSubview(restartButton)
+        view.addSubview(settingsButton)
         
         //button actions
         showButton.addTarget(self, action: #selector(revealButtonTapped(_:)), for: .touchUpInside)
@@ -111,6 +125,22 @@ class ViewController: UIViewController {
             riddleLabel.text = "No riddles available"
             answerLabel.text = ""
         }
+        
+        //Settings Menu
+        let settingsMenu = UIMenu(title: "Postavke", children: [
+            UIAction(title: "Limit Riddles", image: UIImage(systemName: "number")) { _ in
+                print("Limit Riddles tapped")
+            },
+            UIAction(title: "Send Feedback", image: UIImage(systemName: "envelope")) { _ in
+                print("Send Feedback tapped")
+            },
+            UIAction(title: "Language", image: UIImage(systemName: "globe")) { _ in
+                print("Change Language tapped")
+            }
+        ])
+        
+        settingsButton.menu = settingsMenu
+        settingsButton.showsMenuAsPrimaryAction
     }
     
     func loadRiddles() -> [Riddle]? {
@@ -163,61 +193,67 @@ class ViewController: UIViewController {
                 alert.addAction(cancelRestart)
                 self.present(alert, animated: true, completion: nil)
             }}}
-        
-        
-        
-        func showRiddle() {
-            let riddle = riddles[currentRiddleIndex]
-            riddleLabel.text = riddle.question
-            answerLabel.text = "??"
-            isAnswerRevealed = false
-        }
-        
-    @objc func restartButtonTapped(_ sender: UIButton) {
-            let alert = UIAlertController(title: "Da li želite da počnete ispočetka?", message: nil, preferredStyle: .actionSheet)
-            
-            // Add options to the popup menu
-            let acceptRestart = UIAlertAction(title: "Da!", style: .default) { _ in
-                self.currentRiddleIndex = 0
-                self.showRiddle()
-            }
-            
-            let cancelRestart = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            
-            alert.addAction(acceptRestart)
-            alert.addAction(cancelRestart)
-            self.present(alert, animated: true, completion: nil)
-        }
-        
-         private func setupConstraints() {
-            NSLayoutConstraint.activate([
-                restartButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-                restartButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-                restartButton.heightAnchor.constraint(equalToConstant: 40),
-                restartButton.widthAnchor.constraint(equalToConstant: 40),
-
-                riddleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                riddleLabel.topAnchor.constraint(equalTo: restartButton.bottomAnchor, constant: 10),
-                riddleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                riddleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-                
-                imageView.topAnchor.constraint(equalTo: riddleLabel.bottomAnchor, constant: 50),
-                imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                imageView.widthAnchor.constraint(equalToConstant: 300),
-                imageView.heightAnchor.constraint(equalToConstant: 300),
-       
-                answerLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
-                answerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-                showButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                showButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-                showButton.heightAnchor.constraint(equalToConstant: 50),
-                showButton.widthAnchor.constraint(equalToConstant: 120),
-                
-                nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-                nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-                nextButton.heightAnchor.constraint(equalToConstant: 50),
-                nextButton.widthAnchor.constraint(equalToConstant: 120)
-            ])
-        }
+    
+    
+    
+    func showRiddle() {
+        let riddle = riddles[currentRiddleIndex]
+        riddleLabel.text = riddle.question
+        answerLabel.text = "??"
+        isAnswerRevealed = false
     }
+    
+    @objc func restartButtonTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Da li želite da počnete ispočetka?", message: nil, preferredStyle: .actionSheet)
+        
+        // Add options to the popup menu
+        let acceptRestart = UIAlertAction(title: "Da!", style: .default) { _ in
+            self.currentRiddleIndex = 0
+            self.showRiddle()
+        }
+        
+        let cancelRestart = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(acceptRestart)
+        alert.addAction(cancelRestart)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            restartButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            restartButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            restartButton.heightAnchor.constraint(equalToConstant: 40),
+            restartButton.widthAnchor.constraint(equalToConstant: 40),
+            
+            //   Settings Button (Top-Left) - If added later
+            settingsButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            settingsButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            settingsButton.heightAnchor.constraint(equalToConstant: 40),
+            settingsButton.widthAnchor.constraint(equalToConstant: 40),
+            
+            riddleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            riddleLabel.topAnchor.constraint(equalTo: restartButton.bottomAnchor, constant: 10),
+            riddleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            riddleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            imageView.topAnchor.constraint(equalTo: riddleLabel.bottomAnchor, constant: 50),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 300),
+            imageView.heightAnchor.constraint(equalToConstant: 300),
+            
+            answerLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            answerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            showButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            showButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            showButton.heightAnchor.constraint(equalToConstant: 50),
+            showButton.widthAnchor.constraint(equalToConstant: 120),
+            
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            nextButton.heightAnchor.constraint(equalToConstant: 50),
+            nextButton.widthAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+}
